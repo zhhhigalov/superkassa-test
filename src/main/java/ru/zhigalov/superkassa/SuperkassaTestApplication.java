9faafa90143916d9d54083c3471a6b12adcdd25e
Maksim Zhigalov <zhigalovmi@gmail.com>
2023-02-11 16:36:04 +0300

    init

---
+++ src/main/java/ru/zhigalov/superkassa/SuperkassaTestApplication.java
@@ -0,0 +1,13 @@
+package ru.zhigalov.superkassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperkassaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperkassaTestApplication.class, args);
+    }
+
+}
